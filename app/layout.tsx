be8641b964052f6fd7a803f9f1a652453cdcb552
Digital Shreee <109030932+shreeecreation@users.noreply.github.com>
2025-05-29 14:10:48 +0545

    fix : title and keywords

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,27 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Stickify',
+  description: 'Print your ideas into reality with us',
   generator: 'v0.dev',
+  applicationName: 'StickifyNepal',
+  referrer: 'origin-when-cross-origin',
+  keywords: [
+    'stickers',
+    'custom stickers',
+    'print stickers',
+    'sticker printing',
+    'custom printing',
+    'Nepal',
+    'stickifynepal',
+    'stickify',
+    'stickifynepal.com',
+    'stickifynepal.com.np',],
+  authors: [{ name: 'StickifyNepal' }],
+  creator: 'StickifyNepal',
+  publisher: 'StickifyNepal',
+  colorScheme: 'light dark',
+
    icons: {
     icon: 'https://raw.githubusercontent.com/shreeecreation/StickifyImages/refs/heads/main/logo.jpeg', // or .ico, svg, whatever you have in public/
   },
